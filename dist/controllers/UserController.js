22693006e8df5854ec991ab68f26d3b844b79a28
bepyan <65283190+bepyan@users.noreply.github.com>
2021-12-21 01:11:25 +0900

    feat. readd dist

---
+++ dist/controllers/UserController.js
@@ -0,0 +1,67 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteUser = exports.unSubscriptNotion = exports.subscriptNotion = exports.create = exports.findOne = exports.findAll = void 0;
+const express_validator_1 = require("express-validator");
+const middlewares_1 = require("../middlewares");
+const services_1 = require("../services");
+/* ---------------- GET ---------------- */
+const findAll = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield services_1.UserService.findAll();
+    res.send(data);
+});
+exports.findAll = findAll;
+exports.findOne = [
+    (0, express_validator_1.param)("name").notEmpty(),
+    middlewares_1.validatorErrorChecker,
+    (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield services_1.UserService.findUser({ name: req.params.name });
+        res.send(data);
+    }),
+];
+/* ---------------- POST ---------------- */
+exports.create = [
+    (0, express_validator_1.body)("name").notEmpty(),
+    middlewares_1.validatorErrorChecker,
+    (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const user = yield services_1.UserService.createUser(req.body);
+        res.send(user);
+    }),
+];
+exports.subscriptNotion = [
+    (0, express_validator_1.body)("name").notEmpty(),
+    (0, express_validator_1.body)("notion").notEmpty(),
+    middlewares_1.validatorErrorChecker,
+    (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const user = yield services_1.UserService.addNotion(req.body);
+        res.send(user);
+    }),
+];
+exports.unSubscriptNotion = [
+    (0, express_validator_1.body)("name").notEmpty(),
+    (0, express_validator_1.body)("notion").notEmpty(),
+    middlewares_1.validatorErrorChecker,
+    (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const user = yield services_1.UserService.delelteNotion(req.body);
+        res.send(user);
+    }),
+];
+/* ---------------- UPDATE ---------------- */
+/* ---------------- DELETE ---------------- */
+exports.deleteUser = [
+    (0, express_validator_1.body)("name").notEmpty(),
+    middlewares_1.validatorErrorChecker,
+    (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const user = yield services_1.UserService.deleteUser(req.body.name);
+        res.send(user);
+    }),
+];
+//# sourceMappingURL=UserController.js.map
---
+++ dist/routes/users.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const controllers_1 = require("../controllers");
+const router = express_1.default.Router();
+router.get("/", controllers_1.UserController.findAll);
+router.get("/:name", controllers_1.UserController.findOne);
+router.post("/", controllers_1.UserController.create);
+router.post("/notion", controllers_1.UserController.subscriptNotion);
+router.delete("/", controllers_1.UserController.deleteUser);
+router.delete("/notion", controllers_1.UserController.unSubscriptNotion);
+exports.default = router;
+//# sourceMappingURL=users.js.map
